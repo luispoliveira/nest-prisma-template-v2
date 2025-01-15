0ee096f7e7c93af08becf0dcdde374b3dbc72b5e
Luis Oliveira <luispedroliveira96@gmail.com>
2025-01-15 21:44:01 +0000

    refactor: Migrate from bullmq to bull for queue processing and simplify job handling

--- apps/worker/src/consumer/default.consumer.ts
+++ apps/worker/src/consumer/default.consumer.ts
@@ -1,24 +1,15 @@
 import { EVENTS, QUEUES } from '@lib/queue';
-import { Processor, WorkerHost } from '@nestjs/bullmq';
-import { Job } from 'bullmq';
+import { Process, Processor } from '@nestjs/bull';
+import { Job } from 'bull';
 
 @Processor(QUEUES.DEFAULT)
-export class DefaultConsumer extends WorkerHost {
-  async process(job: Job, token?: string): Promise<any> {
-    switch (job.name) {
-      case EVENTS.TEST:
-        await this.test(job);
-        break;
-      case EVENTS.ANOTHER_TEST:
-        await this.anotherTest(job);
-        break;
-    }
-  }
-
+export class DefaultConsumer {
+  @Process(EVENTS.TEST)
   async test(job: Job): Promise<void> {
     console.log('test');
   }
 
+  @Process(EVENTS.ANOTHER_TEST)
   async anotherTest(job: Job): Promise<void> {
     console.log('another test');
   }
