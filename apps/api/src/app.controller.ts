181f067c0caf34b685ef0b57ef01b56e57fc65a3
Luis Oliveira <luispedroliveira96@gmail.com>
2025-05-21 22:32:40 +0100

    refactor(app): integrate ClsService and log user information in getHello method

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,8 +1,9 @@
-import { Public } from "@lib/auth";
+import { LoggedUser } from "@lib/auth";
 import { BaseAuthController } from "@lib/auth/controllers/base-auth.controller";
 import { DefaultJob } from "@lib/queue";
 import { Controller, Get } from "@nestjs/common";
 import { ApiBearerAuth, ApiHeader } from "@nestjs/swagger";
+import { ClsService } from "nestjs-cls";
 import { AppService } from "./app.service";
 
 @ApiHeader({
@@ -15,17 +16,21 @@ export class AppController extends BaseAuthController {
   constructor(
     private readonly appService: AppService,
     private readonly _defaultJob: DefaultJob,
+    private readonly _clsService: ClsService,
   ) {
     super();
   }
 
-  @Public()
+  // @Public()
   @Get()
   async getHello() {
-    await this._defaultJob.addTestJob();
+    // await this._defaultJob.addTestJob();
 
     // wait 5 seconds
     // await new Promise(resolve => setTimeout(resolve, 5000));
+
+    const user = this._clsService.get<LoggedUser>("user");
+    console.log("🚀 ~ AppController ~ getHello ~ user:", user);
 
     await this._defaultJob.addAnotherTestJob();
 
