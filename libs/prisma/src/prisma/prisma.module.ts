e5e46f60d64e8d7f0e4411b4de2ee814d4a2c43e
Luis Oliveira <luispedroliveira96@gmail.com>
2025-06-18 12:46:47 +0100

    feat: add launch configuration for debugging NestJS app and update Prisma module imports

--- libs/prisma/src/prisma/prisma.module.ts
+++ libs/prisma/src/prisma/prisma.module.ts
@@ -1,8 +1,18 @@
 import { Global, Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { configuration } from "../config/configuration";
+import { validationSchema } from "../config/validation";
 import { PrismaService } from "./prisma.service";
 
 @Global()
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      validationSchema,
+    }),
+  ],
   providers: [PrismaService],
   exports: [PrismaService],
 })
